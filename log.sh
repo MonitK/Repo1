f95e1dcbe3977968e4072f615f5c6760e8ad41d0
MonitK <mkapoor@ddn.upes.ac.in>
2018-03-19 11:23:34 +0530

    19th march,before branching

--- log.sh
+++ log.sh
@@ -1,4 +1,4 @@
-git log -n 2
+git log -n 2 > newlog.txt
 #comments for Bug1
 #comments for F05
 #after this hot fix will take over
--- myfirst.c
+++ myfirst.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
-#include<math.h>
+//#include<math.h>
+////commented math.h
 main()
 {
 	printf("helo world");
+	printf("\n new line demo");
+//comment1
+//comment2
+//comment3
 }
+
